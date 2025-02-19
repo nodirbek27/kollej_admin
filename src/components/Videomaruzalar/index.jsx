87d53b2d8b6416aaa78dc587dec02d3e9ee457de
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2025-02-19 09:28:53 +0500

    remove unnecessary files

--- src/components/Videomaruzalar/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Videomaruzalar = () => {
-  return (
-    <div>Videomaruzalar</div>
-  )
-}
-
-export default Videomaruzalar
--- src/pages/Home/InstitutJurnali/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InstitutJurnali = () => {
-  return (
-    <div>InstitutJurnali</div>
-  )
-}
-
-export default InstitutJurnali
